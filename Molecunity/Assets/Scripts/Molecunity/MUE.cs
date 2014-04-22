27a1a5b23ec49fc869d7e94a37a737c6c1c60f2e
Daniel G <daniel.18@xa1.at>
2014-04-22 11:40:14 +0200

    created class basis

---
+++ Molecunity/Assets/Scripts/Molecunity/MUE.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Molecunity Environment
+/// </summary>
+public class MUE : ScriptableObject {
+
+	void OnEnable ()
+	{
+		hideFlags = HideFlags.HideAndDontSave;
+		Debug.Log ("MUE enabled");
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	[SerializeField]
+	private List<MoleculeSpecies> species = new List<MoleculeSpecies>();
+	public MoleculeSpecies[] Species {
+		get {
+			return species.ToArray();
+		}
+	}
+
+	public string TEST;
+
+	public void AddSpecies(MoleculeSpecies s) {
+		species.Add (s);
+	}
+
+	public void RemoveSpecies(MoleculeSpecies s) {
+		species.Remove (s);
+	}
+
+	private static MUE instance;
+	public static MUE GetInstance() {
+		if (instance == null) {
+			Debug.Log ("create MUE on my own");
+			instance = CreateInstance<MUE>();
+		}
+
+		return instance;
+	}
+}
